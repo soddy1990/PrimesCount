ae480c6ba978a23a36def3a9e9c27cf28e8bca75
soddy1990 <soddy1990@gmail.com>
2015-05-06 17:10:59 -0400

    Initial Commit

---
+++ PrimesCount/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PrimesCount
+//
+//  Created by 黄美强 on 15/5/6.
+//  Copyright (c) 2015年 黄 美强. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
